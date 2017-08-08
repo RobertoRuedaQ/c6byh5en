c4d6ac16fcc39ce886cdd2a030c8f488ebff6806
roberto <rueda.roberto89@gmail.com>
2017-08-08 10:13:24 -0500

    added create to controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -6,4 +6,15 @@ class Api::V1::ProductsController < ApplicationController
   render json: @products, status: :ok
   end
   
+
+  def create
+  	@product = Product.new(params[product_params])
+  end
+
+
+  private
+
+  def product_params
+  	params.require(:product).permit(:name, :price)
+  end
 end
